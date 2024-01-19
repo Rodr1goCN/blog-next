7351d85180f77f7d7d2f196b38ff71fc3543ed09
Rodrigo Costa <drigocn2@hotmail.com>
2024-01-18 22:04:33 -0300

    Blog-Next
    Adicionando rotas para visualização do título das postagens do blog

--- src/domain/posts/post.ts
+++ src/domain/posts/post.ts
@@ -1,31 +1,6 @@
-export type PostID = number;
+type PostID = number;
 
-export type PostAuthor = {
-    id: PostID;
-    name: string;
-    created_by: number;
-    updated_by: number;
-    created_at: string;
-    updated_at: string;
-};
-
-export type PostCategory = {
-    id: PostID;
-    name: string;
-    created_by: number;
-    updated_by: number;
-    created_at: string;
-    updated_at: string;
-};
-
-export type PostCreatedBy = {
-    id: PostID;
-    firstname: string;
-    lastname: string;
-    username: null;
-};
-
-export type PostCoverFormat = {
+type PostCoverFormat = {
     ext: string;
     url: string;
     hash: string;
@@ -41,34 +16,69 @@ export type PostCoverFormat = {
     };
 };
 
-export type PostCover = PostCoverFormat & {
-    id: PostID;
+type PostCoverAttributes = PostCoverFormat & {
     alternativeText: string;
     caption: string;
-    previewUrl: null;
-    provider: string;
-    created_by: number;
-    updated_by: number;
-    created_at: string;
-    updated_at: string;
     formats: {
         thumbnail: PostCoverFormat;
         small: PostCoverFormat;
         medium: PostCoverFormat;
         large: PostCoverFormat;
     };
+    previewUrl: null;
+    provider: string;
+    createdAt: string;
+    updatedAt: string;
+};
+
+type PostCover = {
+    data: {
+        id: PostID;
+        attributes: PostCoverAttributes;
+    };
+};
+
+type Attributes = {
+    name: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+};
+
+type PostAuthor = {
+    data: {
+        id: PostID;
+        attributes: Attributes;
+    };
+};
+
+type PostCategory = PostAuthor;
+
+type PostAttributes = {
+    title: string;
+    content: string;
+    slug: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    author: PostAuthor;
+    category: PostCategory;
+    cover: PostCover;
 };
 
 export type PostData = {
     id: PostID;
-    title: string;
-    content: string;
-    slug: string;
-    author: PostAuthor;
-    category: PostCategory;
-    created_by: PostCreatedBy;
-    updated_by: PostCreatedBy;
-    created_at: string;
-    updated_at: string;
-    cover: PostCover;
+    attributes: PostAttributes;
 };
+
+export interface getPostsData {
+    data: PostData[];
+    meta: {
+        pagination: {
+            page: number;
+            pageSize: number;
+            pageCount: number;
+            total: number;
+        };
+    };
+}
