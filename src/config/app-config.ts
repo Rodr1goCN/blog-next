0f892ba287ee18eb671ceb26b5e749c32f8ff986
Rodrigo Costa <drigocn2@hotmail.com>
2024-01-20 00:37:49 -0300

    Blog-Next
    corrigido o problema mencionado antes

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -3,4 +3,3 @@ export const POSTS_URL = `${API_URL}/api/posts-p`;
 
 export const SITE_NAME = 'Rodrigo Costa';
 export const SITE_URL = '';
-console.log(POSTS_URL);
--- src/utils/fetch-json.ts
+++ src/utils/fetch-json.ts
@@ -1,5 +1,5 @@
 export const fetchJson = async <T>(url: string): Promise<T> => {
     const rawData = await fetch(url);
     const jsonData = await rawData.json();
-    return jsonData;
+    return jsonData.data;
 };
