482a7960f6a1023b3a35bd1fd5889f781c3be664
Rodrigo Costa <drigocn2@hotmail.com>
2024-01-22 20:56:23 -0300

    Blog-Next
    Ajustando o Header

--- src/containers/Homepage/index.tsx
+++ src/containers/Homepage/index.tsx
@@ -1,5 +1,6 @@
 import { PostData } from '@/src/domain/posts/post';
 import { Container } from './style';
+import { Header } from '@/src/components/Header';
 
 export type HomePageProps = {
     posts: PostData[];
@@ -7,10 +8,13 @@ export type HomePageProps = {
 
 export default function HomePage({ posts }: HomePageProps) {
     return (
-        <Container>
-            {posts.map((post) => (
-                <h2 key={post.attributes.slug}>{post.attributes.title}</h2>
-            ))}
-        </Container>
+        <>
+            <Header />
+            <Container>
+                {posts.map((post) => (
+                    <h2 key={post.attributes.slug}>{post.attributes.title}</h2>
+                ))}
+            </Container>
+        </>
     );
 }
--- src/style/globalstyles.tsx
+++ src/style/globalstyles.tsx
@@ -1,26 +1,31 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  html,
-  body {
-    background-color: yellowgreen;
-    color: ${({ theme }) => theme.colors.primary};
-    padding: 0;
-    margin: 0;
+    * {
+         outline: none;
+         margin: 0;
+         padding: 0;
+         box-sizing: border-box;
+       }
+
+   body {
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    background-color: yellowgreen;
+    font-size: ${({ theme }) => theme.font.sizes.medium};
+  }
+
+  html {
+    font-size: 62.5%; /* Para poder usar o REM, então 1.5rem é igual 15px */
   }
 
   a {
-    color: inherit;
+    color: ${({ theme }) => theme.colors.secondary};
     text-decoration: none;
-  }
-
-  * {
-    outline: none;
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+    transition: opacity 300ms ease-in-out;
+    &:hover{
+        opacity: .5;
+    }
   }
 `;
 
