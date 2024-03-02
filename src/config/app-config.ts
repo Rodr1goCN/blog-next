a57228a045f695397892e1202bdb7e1e874b1dc9
Rodrigo Costa <drigocn2@hotmail.com>
2024-03-01 21:35:08 -0300

    Blog-Next
    Adicionando a url do blog do netlify

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -2,4 +2,4 @@ export const API_URL = 'https://old-version-strapi.onrender.com';
 export const POSTS_URL = `${API_URL}/posts`;
 
 export const SITE_NAME = 'Rodrigo Costa';
-export const SITE_URL = process.env.SITE_URL || 'htt://localhost:3000';
+export const SITE_URL = 'https://personalblog123.netlify.app';
