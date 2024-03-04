343698d8dd9bfff2c0a7ff7cd184876c19a686cf
Rodrigo Costa <drigocn2@hotmail.com>
2024-03-04 19:10:02 -0300

    Blog-Next
    adicionando o remove-html e adicionando o meta description na homepage

--- src/containers/Homepage/index.tsx
+++ src/containers/Homepage/index.tsx
@@ -1,9 +1,11 @@
+import Head from 'next/head';
 import { PostData } from '@/src/domain/posts/post';
 import { Container } from './style';
 import { Header } from '@/src/components/Header';
 import { MainContainer } from '@/src/components/MainContainer';
 import { PostCard } from '@/src/components/PostCard';
 import { Footer } from '@/src/components/Footer';
+import { SITE_NAME } from '@/src/config/app-config';
 
 export type HomePageProps = {
     posts: PostData[];
@@ -12,6 +14,13 @@ export type HomePageProps = {
 export default function HomePage({ posts }: HomePageProps) {
     return (
         <>
+            <Head>
+                <title>{SITE_NAME}</title>
+                <meta
+                    name="description"
+                    content="Este é o meu primeiro blog de tecnologia"
+                />
+            </Head>
             <Header />
             <MainContainer>
                 <Container>
--- src/containers/Post/index.tsx
+++ src/containers/Post/index.tsx
@@ -7,6 +7,9 @@ import { PostCover } from '@/src/components/PostCover';
 import { PostDetails } from '@/src/components/PostDetails';
 import { PostData } from '@/src/domain/posts/post';
 import { Comments } from '@/src/comments';
+import Head from 'next/head';
+import { SITE_NAME } from '@/src/config/app-config';
+import { removeHtml } from '@/src/utils/remove-html';
 
 export type PostProps = {
     post: PostData;
@@ -15,6 +18,15 @@ export type PostProps = {
 export const Post = ({ post }: PostProps) => {
     return (
         <>
+            <Head>
+                <title>
+                    {post.title} - {SITE_NAME}
+                </title>
+                <meta
+                    name="description"
+                    content={removeHtml(post.content).slice(0, 150)}
+                />
+            </Head>
             <Header />
 
             <MainContainer>
