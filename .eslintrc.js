6c8da0e87859232515f44b3de88f7f6732364ce3
Rodrigo Costa <drigocn2@hotmail.com>
2024-03-15 21:45:57 -0300

    Blog-Next
    Voltando o que era antes de instalar o Storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,40 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true
-  },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
-  overrides: [{
     env: {
-      node: true
+        browser: true,
+        es2021: true,
+        node: true,
     },
-    files: ['.eslintrc.{js,cjs}'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended',
+    ],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parser: '@typescript-eslint/parser',
     parserOptions: {
-      sourceType: 'script'
-    }
-  }],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: ['@typescript-eslint', 'react'],
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  },
-  rules: {
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    'react/react-in-jsx-scope': 'off'
-  }
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint', 'react'],
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    rules: {
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        'react/react-in-jsx-scope': 'off',
+    },
 };
